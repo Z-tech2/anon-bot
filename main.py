68e676be0dbd3ab596308ad1eb90e9f65b756b1c
Z-tech2 <107017335+Z-tech2@users.noreply.github.com>
2024-01-13 20:43:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ token = "PUT BOT TOKEN HERE"
 
 @bot.slash_command(description="Sends messages anonymously")
 async def send(inter, message):
-    await inter.channel.send(message)
+    if len(message) < 2001:
+     await inter.channel.send(message)
 
 bot.run(token)
